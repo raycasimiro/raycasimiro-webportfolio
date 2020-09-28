5be542a3ae8abee955b93b21043a15a6c20602c2
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2020-09-28 18:24:02 +0800

    bug fixes

--- script.js
+++ script.js
@@ -2,44 +2,44 @@
 
 
 
-var $grid = $('.grid').imagesLoaded( function() {
-  $grid.isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows
-  });
-});
-
-
-//var $grid = $('.grid').isotope({
- // itemSelector: '.grid-item',
- // layoutMode: 'fitRows'
+//var $grid = $('.grid').imagesLoaded( //function() {
+//$grid.isotope({
+// itemSelector: '.grid-item',
+// layoutMode: 'fitRows'
+// });
 //});
+
+
+var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+});
 
 
 // filter functions
 var filterFns = {
-  // show if number is greater than 50
-  numberGreaterThan50: function() {
-    var number = $(this).find('.number').text();
-    return parseInt( number, 10 ) > 50;
-  },
-  // show if name ends with -ium
-  ium: function() {
-    var name = $(this).find('.name').text();
-    return name.match( /ium$/ );
-  }
+    // show if number is greater than 50
+    numberGreaterThan50: function () {
+        var number = $(this).find('.number').text();
+        return parseInt(number, 10) > 50;
+    },
+    // show if name ends with -ium
+    ium: function () {
+        var name = $(this).find('.name').text();
+        return name.match(/ium$/);
+    }
 };
 
-//$( document ).ready(function() {
-//    console.log( "ready!" );
- //    window.location.hash = "filter=*";
-//});
+$( document ).ready(function() {
+   console.log( "ready!" );
+   window.location.hash = "filter=*";
+});
 
 function getHashFilter() {
-  // get filter=filterName
-  var matches = location.hash.match( /filter=([^&]+)/i );
-  var hashFilter = matches && matches[1];
-  return hashFilter && decodeURIComponent( hashFilter );
+    // get filter=filterName
+    var matches = location.hash.match(/filter=([^&]+)/i);
+    var hashFilter = matches && matches[1];
+    return hashFilter && decodeURIComponent(hashFilter);
 }
 
 // init Isotope
@@ -47,39 +47,39 @@ function getHashFilter() {
 
 // bind filter button click
 var $filterButtonGroup = $('.filter-button-group');
-$filterButtonGroup.on( 'click', 'button', function() {
-  var filterAttr = $( this ).attr('data-filter');
-  // set filter in hash
-  location.hash = 'filter=' + encodeURIComponent( filterAttr );
+$filterButtonGroup.on('click', 'button', function () {
+    var filterAttr = $(this).attr('data-filter');
+    // set filter in hash
+    location.hash = 'filter=' + encodeURIComponent(filterAttr);
 });
 
 var isIsotopeInit = false;
 
 function onHashchange() {
-  var hashFilter = getHashFilter();
-  if ( !hashFilter && isIsotopeInit ) {
-    return;
-  }
-  isIsotopeInit = true;
-  // filter isotope
-  $grid.isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows',
-    // use filterFns
-    filter: filterFns[ hashFilter ] || hashFilter
-  });
-  // set selected class on button
-  if ( hashFilter ) {
-    $filterButtonGroup.find('.is-checked').removeClass('is-checked');
-    $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
-  }
+    var hashFilter = getHashFilter();
+    if (!hashFilter && isIsotopeInit) {
+        return;
+    }
+    isIsotopeInit = true;
+    // filter isotope
+    $grid.isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        // use filterFns
+        filter: filterFns[hashFilter] || hashFilter
+    });
+    // set selected class on button
+    if (hashFilter) {
+        $filterButtonGroup.find('.is-checked').removeClass('is-checked');
+        $filterButtonGroup.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
+    }
 }
 
-$(window).on( 'hashchange', onHashchange );
+$(window).on('hashchange', onHashchange);
 
 // trigger event handler to init Isotope
 onHashchange();
 
-$('.navbar-nav>li>a').on('click', function(){
+$('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
