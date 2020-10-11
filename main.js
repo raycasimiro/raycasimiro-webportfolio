aa0a202d4a4e9a3d0f8391418b3a1e5c36c28e49
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2020-10-11 17:39:39 +0800

    new updates on category selection. removed dropdown items on navbar

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
-$(".scrollable").on('click','span', function(){
+$(".scrollable").on('click', 'span', function () {
     if (!$(this).hasClass('category-item-selected'))
-$(this).toggleClass('category-item-selected').siblings().removeClass('category-item-selected');
+        $(this).toggleClass('category-item-selected').siblings().removeClass('category-item-selected');
+
+    if ($(this).is(':nth-last-child(2)'))
+        $(".catergory-overlay-right").hide();
+    else
+        $(".catergory-overlay-right").show();
 })
