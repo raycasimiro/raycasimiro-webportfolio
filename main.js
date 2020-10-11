5908e3c5bd836db2c685ebdc5ddb370850fc1fe7
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2020-10-11 20:04:07 +0800

    update border color

--- main.js
+++ main.js
@@ -5,9 +5,4 @@ $('.navbar-nav>li>a').on('click', function () {
 $(".scrollable").on('click', 'span', function () {
     if (!$(this).hasClass('category-item-selected'))
         $(this).toggleClass('category-item-selected').siblings().removeClass('category-item-selected');
-
-    if ($(this).is(':nth-last-child(2)'))
-        $(".catergory-overlay-right").hide();
-    else
-        $(".catergory-overlay-right").show();
 })
