55e8a7abe57514f947af403cae3ccf6cf7aec1fe
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2020-11-01 23:12:36 +0800

    added about and contact panels

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ $(".scrollable").on('click', 'span', function () {
     if (!$(this).hasClass('category-item-selected'))
         $(this).toggleClass('category-item-selected').siblings().removeClass('category-item-selected');
 })
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
